5be5f7e3b177425e6999b9973c3c18217d9149e3
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2021-08-07 23:30:15 +0200

    added 2 tests

--- 03_jenkins101/src/test/java/github/GitHubSettings.java
+++ 03_jenkins101/src/test/java/github/GitHubSettings.java
@@ -4,7 +4,7 @@ package github;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
-public class GitHubRepository {
+public class GitHubSettings {
 
     @Test
     void justDoSomeTime() {
